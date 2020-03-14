2d8b48995cd168057e3327b210ed12c1a0b59e9d
David J. Straley <djstraley@sandyhook.com>
2020-03-14 17:14:27 -0500

    customied for Coffee drinkers

--- WiredBrainCoffee/Program.cs
+++ WiredBrainCoffee/Program.cs
@@ -6,7 +6,7 @@ namespace WiredBrainCoffee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wired Brain!");
+            Console.WriteLine("Wired Brain Coffee!");
         }
     }
 }
